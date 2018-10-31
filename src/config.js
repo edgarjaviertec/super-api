4aa4929351c5c4bcb3606c7a591815bb7a59638e
Edgar Tec <edgartec87@gmail.com>
2018-10-31 17:32:00 -0500

    Add secret key and expiration time to the config file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
+    accessTokenSecret: "CLAVE_SECRETA2501",
+    accessTokenLife: 86400,
     port: process.env.PORT || 3001,
-    db: process.env.MONGODB_URI || 'mongodb://localhost:27017/shop',
-    SECRET_TOKEN: 'miclavedetokens'
-}
+};
