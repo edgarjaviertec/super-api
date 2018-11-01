a78c206308fd3c753c1087bb4cc444972ec0f81b
Edgar Tec <edgartec87@gmail.com>
2018-11-01 14:59:05 -0500

    Add update validation to permission controller

--- src/validations/permissionValidation.js
+++ src/validations/permissionValidation.js
@@ -3,7 +3,21 @@ import Joi from 'joi';
 export default {
     create: {
         body: {
-            // La expresión regular valida que el nombre del role tenga un formato tipo slug
+            name: Joi.string().min(3).max(75).required().regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/).options({
+                language: {
+                    string: {
+                        regex: {
+                            base: 'must be a valid slug',
+                        }
+                    }
+                }
+            }),
+            displayName: Joi.string().min(3).max(100).required(),
+            description: Joi.string().min(3).max(255).required(),
+        }
+    },
+    update: {
+        body: {
             name: Joi.string().min(3).max(75).required().regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/).options({
                 language: {
                     string: {
