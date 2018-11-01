6149479846bbb96c68efb7a0590dcf2bce92c4e9
Edgar Tec <edgartec87@gmail.com>
2018-11-01 18:43:40 -0500

    Add register validation

--- src/validations/userValidation.js
+++ src/validations/userValidation.js
@@ -1,6 +1,25 @@
 import Joi from 'joi';
 
 export default {
+    register: {
+        body: {
+            username: Joi.string().alphanum().min(3).max(30).required(),
+            email: Joi.string().email({minDomainAtoms: 2}).required(),
+            password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
+            confirmPassword: Joi.string().required().valid(Joi.ref('password')).options({
+                language: {
+                    any: {
+                        allowOnly: 'Passwords do not match',
+                    }
+                }
+            }),
+            roles: Joi.array().required().items(
+                Joi.number().integer().min(0)
+            )
+
+        }
+    },
+
     create: {
         body: {
             username: Joi.string().alphanum().min(3).max(30).required(),
