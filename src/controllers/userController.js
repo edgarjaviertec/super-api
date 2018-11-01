80778245e2e9e5a54ad8cee5fb64f6f6c003c2de
Edgar Tec <edgartec87@gmail.com>
2018-11-01 12:41:50 -0500

    Add CRUD methods to userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,54 +2,9 @@ import models from "../models";
 import sequelize from "../db";
 import bcrypt from "bcrypt";
 
-function generateError(statusCode, field, location, errorMessagesArray) {
-    let clientErrorResponses = {
-        400: "Bad Request",
-        401: "Unauthorized",
-        402: "Payment Required",
-        403: "Forbidden",
-        404: "Not Found",
-        405: "Method Not Allowed",
-        406: "Not Acceptable",
-        407: "Proxy Authentication Required",
-        408: "Request Timeout",
-        409: "Conflict",
-        410: "Gone",
-        411: "Length Required",
-        412: "Precondition Failed",
-        413: "Payload Too Large",
-        414: "URI Too Long",
-        415: "Unsupported Media Type",
-        416: "Requested Range Not Satisfiable",
-        417: "Expectation Failed",
-        418: "I'm a teapot",
-        421: "Misdirected Request",
-        422: "Unprocessable Entity (WebDAV)",
-        423: "Locked (WebDAV)",
-        424: "Failed Dependency (WebDAV)",
-        426: "Upgrade Required",
-        428: "Precondition Required",
-        429: "Too Many Requests",
-        431: "Request Header Fields Too Large",
-        451: "Unavailable For Legal Reasons",
-    };
-    let error = {};
-    error.statusCode = statusCode;
-    error.statusText = clientErrorResponses[statusCode];
-    error.errors = [
-        {
-            "field": field,
-            "location": location,
-            "messages": errorMessagesArray
-        }
-    ];
-    return error
-}
-
 function encryptPassword(password) {
     return bcrypt.hash(password, 10)
 }
-
 
 function create(req, res) {
     let createUser = (userData) => {
@@ -251,9 +206,72 @@ function deleteRole(req, res) {
 
 }
 
+function read(req, res) {
+    let userId = req.params.id;
+    models.User.findOne({
+        where: {
+            id: userId
+        }
+    }).then((user) => {
+        res.status(200).send(user);
+    }).catch((error) => {
+        res.status(500).send(error);
+    })
+}
+
+function list(req, res) {
+    models.User.findAll().then((users) => {
+        res.status(200).send(users);
+    }).catch((error) => {
+        res.status(500).send(error);
+    })
+}
+
+function update(req, res) {
+    let userId = req.params.id;
+    let updateValues = {
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    };
+    models.User.update(updateValues, {
+        where: {
+            id: userId
+        },
+        returning: true,
+        plain: true
+    }).then(() => {
+        return models.User.findOne({
+            where: {
+                id: userId
+            }
+        });
+    }).then((updated) => {
+        res.status(200).send(updated);
+    }).catch((error) => {
+        res.status(500).send(error);
+    });
+}
+
+function remove(req, res) {
+    let userId = req.params.id;
+    models.User.destroy({
+        where: {
+            id: userId
+        }
+    }).then(() => {
+        res.status(204).send();
+    }).catch((error) => {
+        res.status(500).send(error);
+    })
+}
 
 module.exports = {
+    list: list,
     create: create,
+    read: read,
+    update: update,
+    remove: remove,
     addRole: addRole,
     deleteRole: deleteRole
 };
