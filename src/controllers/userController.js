fd552775540f2e4cac20ae5906f10031fef6c67e
Edgar Tec <edgartec87@gmail.com>
2018-11-01 18:43:28 -0500

    Add register method to user controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,35 +7,7 @@ function encryptPassword(password) {
 }
 
 function create(req, res) {
-    let createUser = (userData) => {
-        return models.User.create(userData)
-    };
-    let getRoles = models.Role.findAll({
-        where: {
-            name: {
-                [sequelize.Op.or]: ['user']
-            }
-        }
-    });
-    encryptPassword(req.body.password).then((hashedPassword) => {
-        return hashedPassword;
-    }).then((hashedPassword) => {
-        req.body.password = hashedPassword;
-        return Promise.all([createUser(req.body), getRoles])
-    }).then((values) => {
-        const [user, role] = values;
-        user.setRoles(role);
-        res.status(201).send(user);
-    }).catch((error) => {
-        res.status(500).send(error);
-    });
-}
-
-function addRole(req, res) {
-
-    let userId = req.params.id;
     let requestedRoles = req.body.roles;
-
     let countRoles = requestedRoles.map((role) => {
         return models.Role.count({
             where: {
@@ -43,15 +15,71 @@ function addRole(req, res) {
             }
         })
     });
+    let createUser = (userData) => {
+        return models.User.create(userData)
+    };
+    let getRoles = models.Role.findAll({
+        where: {
+            id: {
+                [sequelize.Op.or]: requestedRoles
+            }
+        }
+    });
+    Promise.all(countRoles).then((countedRoles) => {
+        let errorMessages = [];
+        countedRoles.forEach((countedRole, index) => {
+            if (countedRole === 0) {
+                errorMessages.push("role with id " + requestedRoles[index] + " does not exist");
+            }
+        });
+        return errorMessages;
+    }).then((errorMessages) => {
+        if (errorMessages.length > 0) {
+            let error = {
+                statusCode: 400,
+                messages: errorMessages
+            };
+            throw error;
+        }
+    }).then(() => {
+        return encryptPassword(req.body.password);
+    }).then((hashedPassword) => {
+        console.log(JSON.stringify(hashedPassword));
+        return hashedPassword;
+    }).then((hashedPassword) => {
+        req.body.password = hashedPassword;
+        return Promise.all([createUser(req.body), getRoles])
+    }).then((values) => {
+        const [user, roles] = values;
+        user.setRoles(roles);
+        res.status(201).send(user);
+    }).catch((error) => {
+        if (error.statusCode) {
+            res.status(error.statusCode).send({
+                messages: error.messages
+            });
+        } else {
+            res.status(500).send(error);
+        }
+    });
+}
 
+function addRole(req, res) {
+    let userId = req.params.id;
+    let requestedRoles = req.body.roles;
+    let countRoles = requestedRoles.map((role) => {
+        return models.Role.count({
+            where: {
+                id: role
+            }
+        })
+    });
     let getUser = models.User.findOne({
         attributes: ['id'],
         where: {
             id: userId
         }
     });
-
-
     let getRoles = models.Role.findAll({
         attributes: ['id', 'name', 'displayName'],
         where: {
@@ -60,8 +88,6 @@ function addRole(req, res) {
             }
         }
     });
-
-
     Promise.all(countRoles).then((countedRoles) => {
         let nonExistentElementsErrorMessages = [];
         // Revisamos si los roles que nos están pidiendo existen en la base de datos
@@ -94,10 +120,6 @@ function addRole(req, res) {
                 throw errorMessages;
             }
         }).then(() => {
-
-
-
-            // res.status(200).send("todo OK");
             // Si no hay algún problema con las validaciones anteriores entonces quitamos los roles solicitados
             Promise.all([getUser, getRoles]).then((results) => {
                 const [user, roles] = results;
@@ -106,17 +128,13 @@ function addRole(req, res) {
             }).catch((error) => {
                 res.status(500).send(error);
             })
-
-
         }).catch((error) => {
             res.status(400).send(error);
         });
     })
-
 }
 
 function deleteRole(req, res) {
-
     let userId = req.params.id;
     let requestedRoles = req.body.roles;
     let countRoles = requestedRoles.map((role) => {
@@ -140,11 +158,9 @@ function deleteRole(req, res) {
             }
         }
     });
-
     Promise.all(countRoles).then((countedRoles) => {
         let nonExistentElementsErrorMessages = [];
         let nonExistentElements = [];
-
         // Si al contar los roles que consultamos alguno devuelve cero, entonces ese rol no existe en la base de datos
         countedRoles.forEach((countedRole, index) => {
             if (countedRole === 0) {
@@ -152,25 +168,18 @@ function deleteRole(req, res) {
                 nonExistentElements.push(requestedRoles[index]);
             }
         });
-
         return [nonExistentElements, nonExistentElementsErrorMessages];
-
     }).then((results) => {
-
         const [nonExistentElements, nonExistentElementsErrorMessages] = results;
-
         let errorMessages = [...nonExistentElementsErrorMessages];
-
         // Obtenemos todos los roles del usuario con su Id
         getUser.then((user) => {
             return user.getRoles();
         }).then((results) => {
             let userRoles = [];
-
             results.forEach((userRole) => {
                 userRoles.push(userRole.id)
             });
-
             // Revisamos si los roles que solicitan son los mismos que el usuario ya tiene asignado
             requestedRoles.forEach((requestedRole) => {
                 if (!userRoles.includes(requestedRole) && !nonExistentElements.includes(requestedRole)) {
@@ -179,16 +188,13 @@ function deleteRole(req, res) {
                     );
                 }
             });
-
             // Si el arreglo de mensajes de error no esta vacío entonces lanzamos una excepción para terminar la promesa
             if (errorMessages.length > 0) {
                 throw errorMessages;
             }
 
         }).then(() => {
-
             // Si no hay algún problema con las validaciones anteriores entonces quitamos los roles solicitados
-
             Promise.all([getUser, getRoles]).then((results) => {
                 const [user, roles] = results;
                 user.removeRoles(roles);
@@ -197,13 +203,10 @@ function deleteRole(req, res) {
             }).catch((error) => {
                 res.status(500).send(error);
             })
-
         }).catch((error) => {
             res.status(400).send(error);
         });
-
     })
-
 }
 
 function read(req, res) {
@@ -234,12 +237,17 @@ function update(req, res) {
         email: req.body.email,
         password: req.body.password
     };
-    models.User.update(updateValues, {
-        where: {
-            id: userId
-        },
-        returning: true,
-        plain: true
+    encryptPassword(req.body.password).then((hashedPassword) => {
+        return hashedPassword;
+    }).then((hashedPassword) => {
+        updateValues.password = hashedPassword;
+        return models.User.update(updateValues, {
+            where: {
+                id: userId
+            },
+            returning: true,
+            plain: true
+        })
     }).then(() => {
         return models.User.findOne({
             where: {
@@ -266,7 +274,33 @@ function remove(req, res) {
     })
 }
 
+function register(req, res) {
+    let createUser = (userData) => {
+        return models.User.create(userData)
+    };
+    let getRoles = models.Role.findAll({
+        where: {
+            name: {
+                [sequelize.Op.or]: ['user']
+            }
+        }
+    });
+    encryptPassword(req.body.password).then((hashedPassword) => {
+        return hashedPassword;
+    }).then((hashedPassword) => {
+        req.body.password = hashedPassword;
+        return Promise.all([createUser(req.body), getRoles])
+    }).then((values) => {
+        const [user, role] = values;
+        user.setRoles(role);
+        res.status(201).send(user);
+    }).catch((error) => {
+        res.status(500).send(error);
+    });
+}
+
 module.exports = {
+    register: register,
     list: list,
     create: create,
     read: read,
