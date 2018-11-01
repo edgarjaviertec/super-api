651f8ad31d3af4dac71a3f7eb9ca7cf6227a39cb
Edgar Tec <edgartec87@gmail.com>
2018-11-01 14:59:23 -0500

    Add update validation to role controller

--- src/validations/roleValidation.js
+++ src/validations/roleValidation.js
@@ -16,7 +16,6 @@ export default {
             description: Joi.string().min(3).max(255).required(),
         }
     },
-
     addOrRemovePermission: {
         body: {
             permissions: Joi.array().required().items(
@@ -24,6 +23,19 @@ export default {
             )
         }
     },
-
-
+    update: {
+        body: {
+            name: Joi.string().min(3).max(75).required().regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/).options({
+                language: {
+                    string: {
+                        regex: {
+                            base: 'must be a valid slug',
+                        }
+                    }
+                }
+            }),
+            displayName: Joi.string().min(3).max(100).required(),
+            description: Joi.string().min(3).max(255).required(),
+        }
+    },
 };
