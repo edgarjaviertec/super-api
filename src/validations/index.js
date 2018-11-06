d2b555c6bbc246bea300d37135a23074ca687b75
Edgar Tec <edgartec87@gmail.com>
2018-11-06 14:52:48 -0500

    Add account validation to index validation

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,9 +1,11 @@
 import user from "./userValidation"
 import role from "./roleValidation"
 import permission from "./permissionValidation"
+import account from "./accountValidation"
 
 module.exports = {
     user: user,
     role: role,
-    permission: permission
+    permission: permission,
+    account: account
 };
