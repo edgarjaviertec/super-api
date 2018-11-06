ca8e3ccb23aa396806a4659e21faf710a7be293f
Edgar Tec <edgartec87@gmail.com>
2018-11-06 14:51:19 -0500

    Add account controller to index controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,8 +2,10 @@ import userController from "./userController"
 import roleController from "./roleController"
 import permissionController from "./permissionController"
 import authController from "./authController"
+import accountController from "./accountController"
 
 module.exports = {
+    account: accountController,
     auth: authController,
     user: userController,
     role: roleController,
